685eef0672250e1296266170e5373ae2d05f6d15
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-07 11:17:15 +0700

    friend function done

--- FriendFunction/FriendFunction.cpp
+++ FriendFunction/FriendFunction.cpp
@@ -2,5 +2,22 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+	string nama;
+
+public:
+	friend void setNama(mahasiswa& a, string);
+};
+
+void setNama(mahasiswa& a, string pNama)
+{
+	a.nama = pNama;
+}
+
+
 int main() {
+	mahasiswa joko;
+	setNama(joko, "Kairi Kumar");
+	return 0;
 }
