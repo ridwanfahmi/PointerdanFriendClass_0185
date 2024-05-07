e62b2856fc38f4c65d5c1a7bcbf7762758c259af
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-07 11:13:39 +0700

    contoh friendfunction

--- FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass.cpp
@@ -12,8 +12,33 @@ public:
 };
 
 class siswa {
+private:
+	int id;
 
+public:
+	void setId(int pId);
+	void displayll(orang& a);
 };
 
+void siswa::displayll(orang& a) {
+	cout << id << endl << a.nama;
+}
+
+void orang::setName(string pNama) {
+nama: pNama;
+}
+
+void siswa::setId(int pId) {
+	id = pId;
+}
+
+
+
 int main() {
+	orang joko;
+	joko.setName("Joko Susilo");
+	siswa joko_siswa;
+	joko_siswa.setId(1);
+	joko_siswa.displayll(joko);
+	return 0;
 }
