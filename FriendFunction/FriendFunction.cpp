6d8753807ffcbe6b87f96f88a1e415d31117a58f
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-07 11:43:38 +0700

    mengubah isi void setNama

--- FriendFunction/FriendFunction.cpp
+++ FriendFunction/FriendFunction.cpp
@@ -10,9 +10,11 @@ public:
 	friend void setNama(mahasiswa& a, string);
 };
 
-void setNama(mahasiswa& a, string pNama)
+void setNama(mahasiswa& a, string b)
 {
-	a.nama = pNama;
+	a.nama = b;
+	cout << a.nama;
+		
 }
 
 
